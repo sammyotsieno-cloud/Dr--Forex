06e84aeb22610e980ac7d3013da7a07d57a1fd81
sammyotsieno-cloud <sammyotsieno@gmail.com>
2026-08-25 09:10:11 +0300

    build: remove Firebase dependencies and downgrade test SDK
    Removed Firebase integration and the Google Services plugin. Downgraded test SDK versions to 34 for broader compatibility and updated Gradle JVM arguments to include headless mode.

--- app/src/test/java/com/example/DrForexPhase1Test.kt
+++ app/src/test/java/com/example/DrForexPhase1Test.kt
@@ -30,7 +30,7 @@ import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [36])
+@Config(sdk = [34])
 class DrForexPhase1Test {
 
     private lateinit var database: DrForexDatabase
